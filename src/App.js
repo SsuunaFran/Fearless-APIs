d10d218c227a239964fc63d82bf6a9a017c80e30
SsuunaFran <ssuunafran@gmail.com>
2023-10-19 10:34:16 -0400

    Removed Home, About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
 import Univeristies from "./Pages/Universities";
 import Layout from "./layouts/Layout";
 import Trends from "./Pages/Trends";
@@ -10,8 +8,6 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/universities" element={<Univeristies />} />
           <Route path="/trends" element={<Trends />} />
           <Route path="*" element={<h1>Not Found</h1>} />
