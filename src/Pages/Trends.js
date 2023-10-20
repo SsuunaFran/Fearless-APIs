c668ec8f6d64d43fc7b1582c8957683625b1b2be
SsuunaFran <ssuunafran@gmail.com>
2023-10-20 02:37:53 -0400

    Added search bar and created links

--- src/Pages/Trends.js
+++ src/Pages/Trends.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import ReactWordcloud from "react-wordcloud";
+import SearchBar from "../components/search";
 
 function Trends() {
   const [trends, setTrends] = useState([]);
   useEffect(() => {
     async function getTrends() {
       const response = await fetch(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=200&regionCode=UG&key=AIzaSyCMKJqq4Hyfbh4rR5peZ8VGFiOf25ZynxY"
+        "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&chart=mostPopular&maxResults=50&regionCode=UG&key=AIzaSyCMKJqq4Hyfbh4rR5peZ8VGFiOf25ZynxY"
       );
       const data = await response.json();
-      // Attaching data to word cloud libnrary
-      const wordCloudData = data.items.map((video, index) => {
-        return { text: video.snippet.title, value: 600 - 4 - index };
+      const wordCloudData = data.items.map((video,index) => {
+        return {
+          text: video.snippet.title,
+          url: `https://www.youtube.com/watch?v=${video.id}`,
+          value: video.statistics.viewCount,
+          index:index
+        };
       });
       setTrends(wordCloudData);
     }
@@ -21,10 +26,25 @@ function Trends() {
   const options = {
     rotations: 0,
   };
+
+  const callbacks = {
+    onWordClick: (word) => {
+      if (word.url) {
+        window.open(word.url, "_blank");
+      }
+    },
+    getWordTooltip: (word) => {
+      const tooltipContent = `${word.value}`;
+      return tooltipContent;
+    },
+  };
+  
+  
   return (
     <>
-      <h1> Trends </h1>
-      <ReactWordcloud words={trends} options={options} />
+      <h1>Trends</h1>
+      <SearchBar></SearchBar>
+      <ReactWordcloud words={trends} options={options} callbacks={callbacks} />
     </>
   );
 }
