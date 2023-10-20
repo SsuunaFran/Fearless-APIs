d0b27664af4038d64263a5bf3fb243c04182fe23
SsuunaFran <ssuunafran@gmail.com>
2023-10-20 12:57:34 -0400

    created Search

--- src/components/search.js
+++ src/components/search.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function SearchBar() {
+    const [selectRegion, setselectRegion] = useState('');
+
+    const handleRegionChange = (e) => {
+        setselectRegion(e.target.value)
+    }
+
+    const search = () => {
+        console.log(selectRegion)
+    }
     return (
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <div className="col-md-6">
                     <div className="input-group mb-3">
-                        <select class="form-select me-4" aria-label="Search">
+                        <select className="form-select me-4" aria-label="Search" onChange={handleRegionChange} value={selectRegion}>
                             <option selected hidden>Search By Region</option>
                             <option value="UG">Uganda</option>
                             <option value="GB">United Kingdom</option>
                             <option value="BE">Belgium</option>
                         </select>
-                        <button className="btn btn-primary" type="submit">
+                        <button className="btn btn-primary" type="submit" onClick={search}>
                             Search
                         </button>
                     </div>
